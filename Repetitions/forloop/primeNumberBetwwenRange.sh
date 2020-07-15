231d6365bde605761bd0055466ad93a379db39e1
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-16 00:06:40 +0530

    ADDED:to find the prime number between given range.

---
+++ Repetitions/forloop/primeNumberBetwwenRange.sh
@@ -0,0 +1,23 @@
+#Extend the program to take a range of number as input and output the Prime Numbers in that range.
+#!/bin/bash -x
+echo "enter the starting range";
+read start;
+echo "entr the ending range";
+read end;
+for ((j=$start;j<=$end;j++))
+do
+count=0
+for ((i=2;i<$j;i++))
+do
+if [ `expr $j % $i` -eq 0 ]
+then
+count=`expr $count + 1 `
+fi
+done
+if [ $count -eq 0 ]
+then 
+echo primenumber$j
+else
+echo notprime$j
+fi
+done
